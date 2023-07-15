e4fadeeb08937149527b020345349936f9ab7836
BRANYA <tbd2504@gmail.com>
2023-07-15 13:56:44 +0300

    Create simple bot

---
+++ warframe_bot/bot.py
@@ -0,0 +1,15 @@
+import disnake
+from disnake.ext import commands
+
+from warframe_bot import config
+
+bot = commands.InteractionBot(intents=disnake.Intents.all())
+
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user.name} is ready!')
+
+
+if __name__ == '__main__':
+    bot.run(config.BOT_TOKEN)
