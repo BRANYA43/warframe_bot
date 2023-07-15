5d08ffe764746865b8b26838e3d32e2c303f1e71
BRANYA <tbd2504@gmail.com>
2023-07-15 16:36:08 +0300

    Refactor tests

--- tests/test_objects/test_base.py
+++ tests/test_objects/test_base.py
@@ -9,44 +9,44 @@ class TestBase(unittest.TestCase):
 
     def setUp(self) -> None:
         self.expiry = datetime.datetime.utcnow()
+        self.base = Base(name='name', title='title', expiry=self.expiry)
 
-    def test_create_base_with_correct_attrs(self):
+    def test_create_base_with_correct_values(self):
         """Test: create base with correct attrs"""
-        base = Base(name='name', title='title', expiry=self.expiry)
-        self.assertEqual(base.name, 'name')
-        self.assertEqual(base.title, 'title')
-        self.assertEqual(base.expiry, self.expiry)
+        self.assertEqual(self.base.name, 'name')
+        self.assertEqual(self.base.title, 'title')
+        self.assertEqual(self.base.expiry, self.expiry)
 
-    def test_not_create_base_with_incorrect_attrs(self):
+    def test_not_create_base_with_incorrect_values(self):
         """Test: not create base with incorrect attrs"""
         with self.assertRaises((TypeError, ValueError)):
-            base = Base(name=None, title=None, expiry=None)
-            base = Base(name='', title='', expiry=None)
+            Base(name=None, title=None, expiry=None)
+            Base(name='', title='', expiry=None)
 
     def test_raise_errors_of_name_property(self):
         """Test: raise ValueError if name is empty string, raise TypeError if name isn't str."""
         with self.assertRaises(TypeError) as e:
-            base = Base(name=None, title='title', expiry=self.expiry)
+            self.base.name = None
         self.assertEqual(str(e.exception), 'Name must be str.')
 
         with self.assertRaises(ValueError) as e:
-            base = Base(name='', title='title', expiry=self.expiry)
+            self.base.name = ''
         self.assertEqual(str(e.exception), 'Name cannot be empty string.')
 
     def test_raise_errors_of_title_property(self):
         """Test: raise ValueError if title is empty string, raise TypeError if title isn't str."""
         with self.assertRaises(TypeError) as e:
-            base = Base(name='name', title=None, expiry=self.expiry)
+            self.base.title = None
         self.assertEqual(str(e.exception), 'Title must be str.')
 
         with self.assertRaises(ValueError) as e:
-            base = Base(name='name', title='', expiry=self.expiry)
+            self.base.title = ''
         self.assertEqual(str(e.exception), 'Title cannot be empty string.')
 
     def test_raise_errors_of_expiry_property(self):
         """Test: raise TypeError if expiry isn't datetime."""
         with self.assertRaises(TypeError) as e:
-            base = Base(name='name', title='title', expiry=None)
+            self.base.expiry = None
         self.assertEqual(str(e.exception), 'Expiry must be datetime.')
 
 
--- tests/test_objects/test_cycle.py
+++ tests/test_objects/test_cycle.py
@@ -8,23 +8,34 @@ class TestBase(unittest.TestCase):
 
     def setUp(self) -> None:
         self.expiry = datetime.datetime.utcnow()
+        self.cycle = Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry,
+                           current_cycle='cycle_1', cycles=['cycle_1', 'cycle_2'])
 
-    def test_create_cycle_with_correct_attrs(self):
+    def test_create_cycle_with_correct_values(self):
         """Test: create cycle with correct attrs."""
-        cycle = Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry,
-                      current_cycle='cycle_1', cycles=['cycle_1', 'cycle_2'])
-        self.assertEqual(cycle.name, 'cycle_name')
-        self.assertEqual(cycle.title, 'cycle_title')
-        self.assertEqual(cycle.current_cycle, 'cycle_1')
-        self.assertEqual(cycle.cycles, ['cycle_1', 'cycle_2'])
+        self.assertEqual(self.cycle.name, 'cycle_name')
+        self.assertEqual(self.cycle.title, 'cycle_title')
+        self.assertEqual(self.cycle.current_cycle, 'cycle_1')
+        self.assertEqual(self.cycle.cycles, ['cycle_1', 'cycle_2'])
 
-    def test_not_create_cycle_with_incorrect_attrs(self):
+    def test_not_create_cycle_with_incorrect_values(self):
         """Test: not create cycle with incorrect attrs"""
         with self.assertRaises((TypeError, ValueError)):
-            cycle = Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry,
-                          current_cycle=None, cycles=None)
-            cycle = Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry,
-                          current_cycle='', cycles=[None, None])
+            Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry, current_cycle=None,
+                  cycles=['cycle_1', 'cycle_2'])
+            Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry, current_cycle='',
+                  cycles=['cycle_1', 'cycle_2'])
+            Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry, current_cycle='None',
+                  cycles=['cycle_1', 'cycle_2'])
+            Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry, current_cycle='cycle_1', cycles=[])
+            Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry, current_cycle='cycle_1', cycles=[None])
+            Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry, current_cycle='cycle_1', cycles=[''])
+            Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry, current_cycle='cycle_1',
+                  cycles=['cycle_1'])
+            Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry, current_cycle='cycle_1',
+                  cycles=[None, 'cycle_1'])
+            Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry, current_cycle='cycle_1',
+                  cycles=['', 'cycle_1'])
 
     def test_raise_errors_of_current_cycle_property(self):
         """
@@ -32,58 +43,49 @@ class TestBase(unittest.TestCase):
         ValueError if current_cycle isn't in list of cycles.
         """
         with self.assertRaises(TypeError) as e:
-            cycle = Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry,
-                          current_cycle=None, cycles=['cycle_1', 'cycle_2'])
+            self.cycle.current_cycle = None
         self.assertEqual(str(e.exception), 'Current cycle must be str.')
 
         with self.assertRaises(ValueError) as e:
-            cycle = Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry,
-                          current_cycle='', cycles=['cycle_1', 'cycle_2'])
+            self.cycle.current_cycle = ''
         self.assertEqual(str(e.exception), 'Current cycle cannot be empty string.')
 
         with self.assertRaises(ValueError) as e:
-            cycle = Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry,
-                          current_cycle='1_cycle', cycles=['cycle_1', 'cycle_2'])
+            self.cycle.current_cycle = 'None'
         self.assertEqual(str(e.exception), 'Current cycle must be in list of cycles.')
 
     def test_raise_errors_of_cycles_property(self):
         """
         Test: raise TypeError if cycles isn't list, raise ValueError if cycles is empty, raise ValueError if cycles
-        has one item, raise ValueError if items of cycles aren't str
+        has one item, raise ValueError if items of cycles aren't str.
         """
         with self.assertRaises(TypeError) as e:
-            cycle = Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry,
-                          current_cycle='cycle_1', cycles=None)
+            self.cycle.cycles = None
         self.assertEqual(str(e.exception), 'Cycles must be list.')
 
         with self.assertRaises(ValueError) as e:
-            cycle = Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry,
-                          current_cycle='cycle_1', cycles=[])
-            cycle = Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry,
-                          current_cycle='cycle_1', cycles=['cycle_1'])
+            self.cycle.cycles = []
+            self.cycle.cycles = ['cycle_1']
         self.assertEqual(str(e.exception), 'Cycles cannot have less 2 items.')
 
         with self.assertRaises(TypeError) as e:
-            cycle = Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry,
-                          current_cycle='cycle_1', cycles=[None, None])
+            self.cycle.cycles = [None, None]
+            self.cycle.cycles = ['cycle_1', None]
         self.assertEqual(str(e.exception), 'Items of cycles must be str.')
 
         with self.assertRaises(ValueError) as e:
-            cycle = Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry,
-                          current_cycle='cycle_1', cycles=['', ''])
+            self.cycle.cycles = ['', '']
+            self.cycle.cycles = ['cycle_1', '']
         self.assertEqual(str(e.exception), 'Items of cycles cannot be empty string.')
 
     def test_set_next_cycle_when_setting_current_cycle(self):
-        """Test: set next_cycle when setting current_cycle"""
-        cycle = Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry,
-                      current_cycle='cycle_1', cycles=['cycle_1', 'cycle_2'])
-        self.assertEqual(cycle.next_cycle, 'cycle_2')
+        """Test: set next_cycle when setting current_cycle."""
+        self.assertEqual(self.cycle.next_cycle, 'cycle_2')
 
     def test_change_next_cycle_when_setting_current_cycle(self):
-        cycle = Cycle(name='cycle_name', title='cycle_title', expiry=self.expiry,
-                      current_cycle='cycle_1', cycles=['cycle_1', 'cycle_2'])
-        cycle.current_cycle = 'cycle_2'
-        self.assertEqual(cycle.next_cycle, 'cycle_1')
+        """Test: change next_cycle when setting another current_cycle."""
+        self.cycle.current_cycle = 'cycle_2'
+        self.assertEqual(self.cycle.next_cycle, 'cycle_1')
 
 
 if __name__ == '__main__':
