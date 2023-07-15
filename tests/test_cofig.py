a76e0a936675cf6472223ddb534651118efcb4d5
BRANYA <tbd2504@gmail.com>
2023-07-15 13:43:00 +0300

    Set config

---
+++ tests/test_cofig.py
@@ -0,0 +1,26 @@
+import os
+import unittest
+
+from warframe_bot import config
+
+
+class TestConfig(unittest.TestCase):
+    def test_set_BASE_DIR(self):
+        """Test: set BASE_DIR for all projects, dir must equal warframe_bot"""
+        self.assertTrue(os.path.exists(config.BASE_DIR))
+        self.assertEqual(os.path.basename(config.BASE_DIR), 'warframe_bot')
+        self.assertEqual(os.path.basename(os.getcwd()), 'warframe_bot')
+
+    def test_correct_constants(self):
+        """Test: constants is correct. they aren't empty string and None. They are correct type"""
+        self.assertIsNotNone(config.BOT_TOKEN)
+        self.assertIsNotNone(config.GUILD_ID)
+        self.assertIsNotNone(config.CHANNEL_ID)
+
+        self.assertNotEqual(config.BOT_TOKEN, '')
+        self.assertNotEqual(config.GUILD_ID, '')
+        self.assertNotEqual(config.CHANNEL_ID, '')
+
+        self.assertIsInstance(config.BOT_TOKEN, str)
+        self.assertIsInstance(config.GUILD_ID, int)
+        self.assertIsInstance(config.CHANNEL_ID, int)
---
+++ warframe_bot/config.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+# Set BASE_DIR for all project
+BASE_DIR = Path(__file__).resolve().parent
+os.chdir(BASE_DIR)
+
+# Load env
+load_dotenv(BASE_DIR / '../.env')
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+GUILD_ID = int(os.getenv('GUILD_ID'))
+CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
