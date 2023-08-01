8c931f0cad1748b73fd5e3843038c53e3ca1e766
BRANYA <tbd2504@gmail.com>
2023-08-01 16:58:13 +0300

    Refactor architectur projects

--- old/tests/test_objects/base_test.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-
-class BaseTest(unittest.TestCase):
-    """Base Test"""
-
-    def check_error_message(self, exception, message):
-        self.assertEqual(str(exception.exception), message)
--- old/warframe_bot/translater.py
+++
@@ -1,8 +0,0 @@
-import gettext
-
-import config
-
-trans = gettext.translation('bot', localedir=config.BASE_DIR / './lang', languages=[config.CURRENT_LANG])
-trans.install()
-
-get_text = trans.gettext
