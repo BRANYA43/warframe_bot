8f8ba6c4ec6805f7d2be3f443fed31d4cc81d8c7
BRANYA <tbd2504@gmail.com>
2023-08-08 16:52:57 +0300

    Refactor set _cost for Item.

--- src/objects/trader.py
+++ src/objects/trader.py
@@ -10,17 +10,14 @@ class Item(NameMixin):
     def __init__(self, name: str, cost: int):
         super().__init__(name)
 
-        self._set_cost(cost)
+        validate_type(cost, int)
+        if cost < 0:
+            raise ValueError('Cost cannot be less 0.')
+        self._cost = cost
 
     @property
     def cost(self):
         return self._cost
-
-    def _set_cost(self, value: int):
-        validate_type(value, int)
-        if value < 0:
-            raise ValueError('Cost cannot be less 0.')
-        self._cost = value
 
     def get_info(self) -> tuple[str, ...]:
         return (
