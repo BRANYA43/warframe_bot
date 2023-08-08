7836f669bf9c124302fc993226a52493202bba20
BRANYA <tbd2504@gmail.com>
2023-08-08 16:48:41 +0300

    Delete name.setter for NameMixin

--- src/objects/mixins/name_mixin.py
+++ src/objects/mixins/name_mixin.py
@@ -5,13 +5,9 @@ class NameMixin:
     """Name Mixin"""
 
     def __init__(self, name: str):
-        self.name = name
+        validate_is_not_empty_string(name)
+        self._name = name
 
     @property
     def name(self) -> str:
         return self._name
-
-    @name.setter
-    def name(self, value: str):
-        validate_is_not_empty_string(value)
-        self._name = value
--- src/tests/test_objects/test_mixins/test_name_mixin.py
+++ src/tests/test_objects/test_mixins/test_name_mixin.py
@@ -21,27 +21,6 @@ class TestNameMixin(unittest.TestCase):
         self.assertRaisesRegex(TypeError, r'Expected str, but got (.+).', NameMixin, None)
         self.assertRaisesRegex(ValueError, r'Value cannot be empty string.', NameMixin, '')
 
-    def test_set_new_name(self):
-        """Test: set new name."""
-        name_mixin = NameMixin(self.name)
-
-        self.assertEqual(name_mixin.name, self.name)
-
-        new_name = 'new_name'
-        name_mixin.name = new_name
-
-        self.assertEqual(name_mixin.name, new_name)
-
-    def test_not_set_new_name_if_value_is_incorrect(self):
-        """Test: not set new name if value is incorrect."""
-        name_mixin = NameMixin(self.name)
-
-        with self.assertRaisesRegex(TypeError, r'Expected str, but got (.+).'):
-            name_mixin.name = None
-
-        with self.assertRaisesRegex(ValueError, r'Value cannot be empty string.'):
-            name_mixin.name = ''
-
 
 if __name__ == '__main__':
     unittest.main()
